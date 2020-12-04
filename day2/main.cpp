4b1a91b157e539e1206b6b19c76c721ac785d237
smkurosu <skurosu@umich.edu>
2020-12-03 23:07:11 -0500

    day2 part2

--- day2/main.cpp
+++ day2/main.cpp
@@ -9,6 +9,30 @@ using std::string;
 using std::cout;
 using std::endl;
 
+int part1(string code, char target){
+  int num = 0;
+  for (int i = 0; i < code.size(); ++i){
+        if (code[i] == target){
+          ++num;
+        }
+  }
+  return num;
+}
+
+bool part2(string code, int pos1, int pos2, char target){
+
+  if(code[pos1-1] == target && code[pos2-1] != target){
+    return true;
+  }
+  else if(code[pos1-1] != target && code[pos2-1] == target){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+
 int main(){
    std::ifstream fin;
   fin.open("input.txt");
@@ -20,7 +44,8 @@ int main(){
   string min, max;
   string target;
   string code; 
-  int invalid = 0;
+  int oldpolicy = 0;
+  int newpolicy = 0;
   int total = 0;
   while(getline(fin, min, '-')){
     getline(fin, max, ' ');
@@ -28,17 +53,16 @@ int main(){
     getline(fin, code);
     ++total;
     code.erase(std::remove_if(code.begin(), code.end(), isspace), code.end());
-    bool badcode = false;
-    int counter = 0;
-    for (int i = 0; i < code.size(); ++i){
-      if (code[i] == target[0]){
-        ++counter;
-      }
-    }
+    int counter = part1(code, target[0]);
     if(counter < std::stoi(min) || counter > std::stoi(max)) {
-      ++invalid;
+      ++oldpolicy;
+    }
+
+    if (part2(code, std::stoi(min), std::stoi(max), target[0])){
+      ++newpolicy;
     }
   }
-  cout << "Number of valid passwords: " << total -invalid;
+  cout << "[ Part 1 ]Number of valid passwords: " << total -oldpolicy << endl;
+  cout << "[ Part 2 ] Valid passwords with new rule: " << newpolicy;
   return 0;
 }
