4d954faafb01cdacea89cf1fd41c1f342b3d92dd
smkurosu <skurosu@umich.edu>
2020-12-06 00:08:36 -0500

    day3

---
+++ day3/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream> 
+#include <string>
+
+using std::getline;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(int argc, char *argv[]){
+  if (argc < 2) {
+    std::cerr << "Pass in test case." << endl; 
+  }
+  string filename = argv[1];
+  cout << "Running against test: " << filename << endl;
+  
+  std::ifstream fin;
+  fin.open(filename);
+  if (!fin.is_open()){
+    cout << "Error opening file\n";
+    exit(0);
+  }
+  string map;
+  int trees = 0;
+  int place = 0;
+  while(fin >> map){
+    if (place > map.size()){
+      place = 0; 
+    }
+    if (map[place] == '#') { 
+      ++trees;
+      map[place] = 'X';
+    }
+    else{
+      map[place] = 'O';
+    }
+    cout << map << endl;
+    place += 3;
+    }
+    cout << "Number of trees: " << trees << endl;
+  return 0;
+}
