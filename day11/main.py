09065048965ac611f6f4e40d6fd77b217a8b6460
smkurosu <skurosu@umich.edu>
2020-12-12 23:32:52 -0500

    misread directions ...

--- day11/main.py
+++ day11/main.py
@@ -22,65 +22,87 @@ def openFile(file: str):
   print(aisleLength)
   print(baseMap)
 
-def emptySeat(state):
+def printMap(map): 
+  global baseMap
+  liner = ""
+  for i in baseMap: 
+    if i[0] % 10 == 0:
+      print(liner)
+      liner = ""
+    liner = liner + i[1].strip()
+
+def seatStatus(state, adjacent, status):
   """
   Parameter: tuple (position, 'L')
   """
   global numAisles
   global aisleLength
   global baseMap
+  # Base case
+  if adjacent == 0:
+    return False
   # Seat in front 
-  if state + aisleLength < numAisles and baseMap[state + aisleLength][1] == "L":
+  if state+(aisleLength*adjacent)<numAisles and \
+    baseMap[state+(aisleLength*adjacent)][1]==status:
     return True
   # Seat behind
-  if state - aisleLength >= 0 and baseMap[state - aisleLength][1] == "L":
+  if state-(aisleLength*adjacent) >= 0 and \
+    baseMap[state-(aisleLength*adjacent)][1] == status:
     return True
   # Seat left 
-  if state- 1 >= 0 and baseMap[state-1][1] == "L":
+  if state-adjacent >= 0 and baseMap[state-adjacent][1] == status:
     return True
   # Seat Right
-  if state+ 1 < aisleLength and baseMap[state+ 1][1] == "L":
+  if state+ adjacent < aisleLength and baseMap[state+adjacent][1] ==status:
     return True
   # Seat diagnol: back right
-  if state- aisleLength - 1 >= 0 and baseMap[state-aisleLength-1][1] == "L":
+  if state-aisleLength-adjacent >= 0 and \
+    baseMap[state-aisleLength-adjacent][1] ==status:
     return True
   # Seat Diagnol: back left  
-  if state- aisleLength + 1 >= 0 and baseMap[state-aisleLength+1][1] == "L":
+  if state- aisleLength + adjacent >= 0 and \
+    baseMap[state-aisleLength+adjacent][1] ==status:
     return True
   # Seat Diagnol: front right
-  if state+ aisleLength - 1 < numAisles and baseMap[state+aisleLength-1][1] == "L":
+  if state+ aisleLength-adjacent< numAisles and \
+    baseMap[state+ aisleLength-adjacent][1] ==status:
     return True
   # Seat Diagnol: front left
-  if state+ aisleLength + 1 < numAisles and baseMap[state+aisleLength+1][1] == "L":
+  if state+aisleLength+adjacent< numAisles and \
+    baseMap[state+aisleLength+adjacent][1]==status:
     return True
+  if status =='#':
+    print("Recursing ... ")
+    seatStatus(state, adjacent -1, status)
   return False
-
-def occupiedSeat(): 
-  global baseMap
-  global numAisles
-  global aisleLength
 
   
 def applyRules():
   global baseMap
   updateMap = []
+  changed = False
   for seat in baseMap: 
     # Floor: ignore
-    if seat[1] == "L" and emptySeat(seat[0]):
+    if seat[1] == "L" and seatStatus(seat[0], 1, "L"):
       updateMap.append((seat[0],"#"))
-    elif seat[1] == "#" and occupiedSeat(seat[0]): 
+      changed = True
+    elif seat[1] == "#" and seatStatus(seat[0], 4, "#"): 
       updateMap.append((seat[0],"L"))
+      changed = True
     else: 
       updateMap.append(seat)
-  print(updateMap)
+  printMap(updateMap)
+  baseMap = updateMap
+  return changed
    
 def main():
-  
   filename = sys.argv[1]
   print("Reading in file ...")
   openFile(filename)
   print("Applying rules ...")
-  applyRules()
+  while applyRules(): 
+    print("Not stabilized")
+  print("Stabilized!")
   
 
         
